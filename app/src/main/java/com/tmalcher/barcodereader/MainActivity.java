237d0c4c34091a1e0b1cb41aee8ebb1278c7ed05
tmalcher <malcher.tm@gmail.com>
2023-08-22 14:28:03 -0400

    added module opencv v4.6.0

--- app/src/main/java/com/tmalcher/barcodereader/MainActivity.java
+++ app/src/main/java/com/tmalcher/barcodereader/MainActivity.java
@@ -2,12 +2,19 @@ package com.tmalcher.barcodereader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "opencv";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug()) Log.d(TAG, "SUCCESS");
+        else Log.d(TAG, "FAILURE");
     }
 }
