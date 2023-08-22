0c9b09944fe73cf75cfa53a583ea3e7bc4987030
tmalcher <malcher.tm@gmail.com>
2023-08-22 16:39:45 -0400

    started to get preview camera and fixed bug opencv preview camera

--- app/src/main/java/com/tmalcher/barcodereader/MainActivity.java
+++ app/src/main/java/com/tmalcher/barcodereader/MainActivity.java
@@ -1,25 +1,78 @@
 package com.tmalcher.barcodereader;
 
+import android.media.CamcorderProfile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.tmalcher.barcodereader.utils.Request;
 
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Collections;
+import java.util.List;
 
+public class MainActivity extends CameraActivity {
     String TAG = "opencv";
+    CameraBridgeViewBase mCameraBridgeViewBase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //requestpermissions
         Request.permissions(this);
 
-        if(OpenCVLoader.initDebug()) Log.d(TAG, "SUCCESS");
-        else Log.d(TAG, "FAILURE");
+        mCameraBridgeViewBase = findViewById(R.id.cameraView);
+
+        mCameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+
+            }
+
+            @Override
+            public void onCameraViewStopped() {
+
+            }
+
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+                //capture frame and convert to matte alpha RGB
+                return inputFrame.rgba();
+            }
+        });
+
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "SUCCESS");
+            mCameraBridgeViewBase.enableView();
+        } else Log.d(TAG, "FAILURE");
     }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(mCameraBridgeViewBase);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCameraBridgeViewBase.enableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCameraBridgeViewBase.disableView();
+    }
+
 }
